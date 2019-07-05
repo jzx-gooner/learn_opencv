99f5838403ae9ddafbceb3f4c9ac3f0191561cb6
jzx <943719065@qq.com>
2019-07-05 20:45:52 +0800

    add
    add

---
+++ kmeas/include/kmeas.h
@@ -0,0 +1,11 @@
+//
+// Created by jzx on 19-7-4.
+//
+
+#ifndef K_MEANS_KMEAS_H
+#define K_MEANS_KMEAS_H
+
+int kmeas_color();
+int kmeas_segmentation();
+int kmeas_demo();
+#endif //K_MEANS_KMEAS_H
